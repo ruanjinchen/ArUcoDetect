eae6b9738a62ce844a560b948503b93cef68f019
ruanjinchen <ruanjinchen@foxmail.com>
2024-04-20 13:24:51 +0800

    update files

--- 2.detect_aruco.py
+++ 2.detect_aruco.py
@@ -1,17 +1,17 @@
 import numpy as np
+import zmq
 import time
 import cv2
 import cv2.aruco as aruco
 import pyrealsense2 as rs
 import yaml
 
+
 file_path = "./标定文件.yaml"
 
 with open(file_path, "r") as file:
     parameter = yaml.load(file.read(), Loader=yaml.Loader)
-    # data = np.load("./IntrinsicMatrix.npz")
     mtx = parameter['camera_matrix']
-    # mtx = data["arr_0"]
     dist = parameter['dist_coefficients']
     camera_u = parameter['camera_u']
     camera_v = parameter['camera_v']
@@ -26,10 +26,15 @@ pipeline = rs.pipeline()
 config = rs.config()
 # 使用默认的配置,设置彩色流
 config.enable_stream(rs.stream.color, 1920, 1080, rs.format.bgr8, 30)
-
 # 启动图像
 pipeline.start(config)
 
+context = zmq.Context()
+socket = context.socket(zmq.PUB)
+host = "127.0.0.1"
+port = 5000
+address = f"tcp://{host}:{port}"
+socket.bind(address)
 
 # num = 0
 while True:
@@ -61,12 +66,22 @@ while True:
         #        aruco.drawDetectedMarkers(frame, corners) #Draw A square around the markers
 
         for i in range(rvec.shape[0]):
+            rvec_degree = np.rad2deg(rvec[i, :, :])
             cv2.drawFrameAxes(frame, mtx, dist, rvec[i, :, :], tvec[i, :, :], 0.03)
             aruco.drawDetectedMarkers(frame, corners)
         # 显示ID，rvec,tvec, 旋转向量和平移向量
         cv2.putText(frame, "Id: " + str(ids), (10, 40), font, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
-        cv2.putText(frame, "rvec: " + str(rvec[i, :, :]), (10, 60), font, 0.5, (0, 255, 0), 2, cv2.LINE_AA)
-        cv2.putText(frame, "tvec: " + str(tvec[i, :, :]), (10, 80), font, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+        cv2.putText(frame, "rvec: " + str(rvec_degree[ :, :]), (10, 160), font, 1.5, (0, 255, 0), 2, cv2.LINE_AA)
+        cv2.putText(frame, "tvec: " + str(tvec[i, :, :]), (10, 260), font, 1.5, (0, 0, 255), 1, cv2.LINE_AA)
+        data = tvec[i, :, :]
+        # print("tvec:", tvec)
+        # print("rvec_degree:", rvec_degree)
+
+        # Correctly accessing the first element from each nested array and converting each to string
+        # message = ",".join(map(str, [tvec[i, 0, 0], tvec[i, 0, 1], tvec[i, 0, 2], rvec_degree[0, 0], rvec_degree[0, 1], rvec_degree[0, 2]]))
+        #
+        # socket.send_string(message)
+        # time.sleep(1)
 
 
     else:
@@ -75,7 +90,7 @@ while True:
 
     end = time.time()
     # 计算帧率并显示
-    cv2.putText(frame, "rate: " + str(1 / (end - start)), (10, 120), font, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+    cv2.putText(frame, "rate: " + str(1 / (end - start)), (10, 60), font, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
     cv2.imshow("frame", frame)
 
     key = cv2.waitKey(1)
