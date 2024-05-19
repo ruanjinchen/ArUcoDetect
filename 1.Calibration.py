fed653c65b7dee011cc0ab467281fe93d9d3aa68
ruanjinchen <ruanjinchen@foxmail.com>
2024-05-19 15:16:47 +0800

    update files

--- 1.Calibration.py
+++ 1.Calibration.py
@@ -1,7 +1,8 @@
-import cv2
 import numpy as np
 import glob
 import yaml
+import cv2
+
 
 # 找棋盘格角点
 # 设置寻找亚像素角点的参数，采用的停止准则是最大循环次数30和最大误差容限0.001
@@ -15,12 +16,13 @@ objp[:, :2] = np.mgrid[0:w, 0:h].T.reshape(-1, 2)
 objp = objp * 18 / 1000  # 16.2mm,实际大小
 
 # 储存棋盘格角点的世界坐标和图像坐标对
-objpoints = []  # 在世界坐标系中的三维点
-imgpoints = []  # 在图像平面的二维点
+obj_points = []  # 在世界坐标系中的三维点
+img_points = []  # 在图像平面的二维点
 # 加载image文件夹下所有的jpg图像
-images = glob.glob('./image/*.png')  # 拍摄的十几张棋盘图片所在目录
+images = glob.glob('./IMAGE/*.png')  # 拍摄的十几张棋盘图片所在目录
 u, v = 0.0, 0.0
 i = 0
+gray = None
 for frame in images:
     img = cv2.imread(frame)
     # 获取画面中心点
@@ -31,30 +33,31 @@ for frame in images:
     # 找到棋盘格角点
     ret, corners = cv2.findChessboardCorners(gray, (w, h), None)
     # 如果找到足够点对，将其存储起来
-    if ret == True:
+    if ret:
         print("i:", i)
         i = i + 1
         # 在原角点的基础上寻找亚像素角点
         cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
         # 追加进入世界三维点和平面二维点中
-        objpoints.append(objp)
-        imgpoints.append(corners)
+        obj_points.append(objp)
+        img_points.append(corners)
         # 将角点在图像上显示
         cv2.drawChessboardCorners(img, (w, h), corners, ret)
-        cv2.namedWindow('findCorners', cv2.WINDOW_NORMAL)
-        cv2.resizeWindow('findCorners', 1920, 1080)
-        cv2.imshow('findCorners', img)
+        cv2.namedWindow('FindCorners', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('FindCorners', 960, 540)
+        cv2.imshow('FindCorners', img)
         cv2.waitKey(200)
+
 cv2.destroyAllWindows()
 # 标定
 print('正在计算')
 print(u, v)
 
 ret, mtx, dist, rvecs, tvecs = \
-    cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+    cv2.calibrateCamera(obj_points, img_points, gray.shape[::-1], None, None)
 
 # 存储标定文件
-file_path = "./标定文件.yaml"
+file_path = "./calibration_result.yaml"
 mtx_yaml = mtx.tolist()
 dist_yaml = dist.tolist()
 camera_u, camera_v = u, v
@@ -69,5 +72,5 @@ print("dist畸变值:\n", dist)  # 畸变系数   Distortion Coefficients = (k_1
 print("rvecs旋转（向量）外参:\n", rvecs)  # 旋转向量  # 外参数
 print("tvecs平移（向量）外参:\n", tvecs)  # 平移向量  # 外参数
 new_camera_mtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (u, v), 0, (u, v))
-print('new_camera_mtx外参', new_camera_mtx)
+print('New_Camera_Mtx 外参', new_camera_mtx)
 print("写入文件名称:", file_path)
--- 2.ArUco Detect.py
+++ 2.ArUco Detect.py
@@ -1,13 +1,13 @@
-import numpy as np
-import zmq
-import time
-import cv2
 import cv2.aruco as aruco
 import pyrealsense2 as rs
+import numpy as np
 import yaml
+import time
+import cv2
+import zmq
 
 
-file_path = "./标定文件.yaml"
+file_path = "./calibration_result.yaml"
 
 with open(file_path, "r") as file:
     parameter = yaml.load(file.read(), Loader=yaml.Loader)
@@ -18,15 +18,23 @@ with open(file_path, "r") as file:
     mtx = np.array(mtx)
     dist = np.array(dist)
 
-
-font = cv2.FONT_HERSHEY_SIMPLEX  # 设置显示字体
+font = cv2.FONT_HERSHEY_TRIPLEX
 
 pipeline = rs.pipeline()
 config = rs.config()
-# 使用默认的配置,设置彩色流
 config.enable_stream(rs.stream.color, 1920, 1080, rs.format.bgr8, 30)
-# 启动图像
-pipeline.start(config)
+pipeline_profile = pipeline.start(config)
+
+device = pipeline_profile.get_device()
+sensor = device.query_sensors()[1]
+exposure = 100  # 曝光
+gain = 50  # 增益
+sensor.set_option(rs.option.exposure, exposure)
+sensor.set_option(rs.option.gain, gain)
+
+window_name = "frame"
+cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+cv2.resizeWindow(window_name, 960, 540)
 
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
@@ -35,15 +43,11 @@ port = 5000
 address = f"tcp://{host}:{port}"
 socket.bind(address)
 
-# num = 0
 while True:
     start = time.time()
     frames = pipeline.wait_for_frames()
     color_frame = frames.get_color_frame()
     frame = np.asanyarray(color_frame.get_data())
-    # ret, frame = color_frame.read()
-    # operations on the frame come here
-
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_1000)
     parameters = aruco.DetectorParameters()
@@ -56,31 +60,31 @@ while True:
         for i in range(rvec.shape[0]):
             # 对每个检测到的标记处理其旋转向量
             R, _ = cv2.Rodrigues(rvec[i, 0, :])  # 现在 rvec[i, 0, :] 是一个1x3的旋转向量
-
             # 从旋转矩阵计算欧拉角 ZYX
             yaw = np.rad2deg(float(np.arctan2(R[1, 0], R[0, 0])))
             pitch = np.rad2deg(float(np.arctan2(-R[2, 0], np.sqrt(R[2, 1]**2 + R[2, 2]**2))))
             roll = np.rad2deg(float(np.arctan2(R[2, 1], R[2, 2])))
-
-            x = f"{roll:.3f}"
-            y = f"{pitch:.3f}"
-            z = f"{yaw:.3f}"
-
+            rx = f"{roll:.3f}"
+            ry = f"{pitch:.3f}"
+            rz = f"{yaw:.3f}"
             cv2.drawFrameAxes(frame, mtx, dist, rvec[i, 0, :], tvec[i, 0, :], 0.03)
-            aruco.drawDetectedMarkers(frame, corners)
-
-            # 显示ID，rvec,tvec, 旋转向量和平移向量
-            cv2.putText(frame, "Id: " + str(ids[i]), (10, 40), font, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
-            cv2.putText(frame, "rvec: " + x + '  ' + y + '  ' + z + '  ', (10, 160), font, 1.5, (0, 255, 0), 2, cv2.LINE_AA)
-            cv2.putText(frame, "tvec: " + str(tvec[i, 0, :]), (10, 260), font, 1.5, (0, 0, 255), 1, cv2.LINE_AA)
-
-            # 发送数据到socket
-            message = ",".join(map(str, [tvec[i, 0, 0], tvec[i, 0, 1], tvec[i, 0, 2], x, y, z]))
+            # aruco.drawDetectedMarkers(frame, corners)  # 调整不了边框粗细
+            for corner in corners:
+                cv2.polylines(frame, [np.int32(corner)], True, (0, 255, 0), 3)
+            X = tvec[i, 0, :][0] * 1000
+            Y = tvec[i, 0, :][1] * 1000
+            Z = tvec[i, 0, :][2] * 1000
+            cv2.putText(frame, "ID: " + str(ids[i]),
+                        (10, 40), font, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+            cv2.putText(frame, "RPY(Degree): " + rx + ', ' + ry + ', ' + rz,
+                        (10, 160), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
+            cv2.putText(frame, " TRANS(MM): " + f"{X:.3f}" + ', ' + f"{Y:.3f}" + ', ' + f"{Z:.3f}",
+                        (10, 210), font, 1, (0, 0, 255), 2, cv2.LINE_AA)
+            # 使用ZMQ发布数据
+            message = ",".join(map(str, [tvec[i, 0, 0], tvec[i, 0, 1], tvec[i, 0, 2], rx, ry, rz]))
             socket.send_string(message)
 
-
     else:
-
         cv2.putText(frame, "No Ids", (10, 64), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
     end = time.time()
@@ -91,13 +95,10 @@ while True:
     key = cv2.waitKey(1)
 
     if key == 27:  # 按esc键退出
-        print('esc break...')
-        cap.release()
+        print('ESC BREAK...')
         cv2.destroyAllWindows()
         break
 
     if key == ord(' '):  # 按空格键保存
-        #        num = num + 1
-        #        filename = "frames_%s.jpg" % num  # 保存一张图像
         filename = str(time.time())[:10] + ".jpg"
         cv2.imwrite(filename, frame)
